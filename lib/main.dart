4c56144ce694d77fdd4394ffb9898013ba2269ee
dreamworld <cubes.dev13@gmail.com>
2022-08-10 17:39:34 +0530

    commited by utsav

---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  Stripe.publishableKey = "Your_key";
+  await Stripe.instance.applySettings();
+  Stripe.merchantIdentifier = 'any string works';
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  Map<String, dynamic>? paymentIntentData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stripe Demo'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 40),
+              child: GestureDetector(
+                onTap: () {
+                  makePayment();
+                },
+                child: Container(
+                  decoration: BoxDecoration( color: Colors.blue,borderRadius: BorderRadius.circular(12)),
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                    child: Text(
+                      'Make Payment',
+                      style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+
+  Future<void> makePayment() async {
+    print('utsav satani');
+    try {
+      paymentIntentData = await createPaymentIntent('${'20'}', 'GBP'); //json.decode(response.body);
+      // print('Response body==>${response.body.toString()}');
+      await Stripe.instance
+          .initPaymentSheet(
+          paymentSheetParameters: SetupPaymentSheetParameters(
+              paymentIntentClientSecret: paymentIntentData!['client_secret'],
+              // applePay: true,
+              // googlePay: true,
+              // testEnv: true,
+              style: ThemeMode.light,
+              // merchantCountryCode: 'GB',
+              merchantDisplayName: 'ANNIE'))
+          .then((value) {});
+
+      print(paymentIntentData);
+      print('paymentIntentData');
+
+      ///now finally display payment sheeet
+      displayPaymentSheet();
+    } catch (e, s) {
+      print('exception:$e$s');
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      await Stripe.instance
+          .presentPaymentSheet(
+          parameters: PresentPaymentSheetParameters(
+            clientSecret: paymentIntentData!['client_secret'],
+            confirmPayment: true,
+          ))
+          .then((newValue) {
+        print('payment intent' + paymentIntentData!['id'].toString());
+        print('payment intent' + paymentIntentData!['client_secret'].toString());
+        print('payment intent' + paymentIntentData!['amount'].toString());
+        print('payment intent' + paymentIntentData.toString());
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("paid successfully")));
+
+      }).onError((error, stackTrace) {
+        print('Exception/DISPLAYPAYMENTSHEET==> $error $stackTrace');
+      });
+    } on StripeException catch (e) {
+      print('Exception/DISPLAYPAYMENTSHEET==> $e');
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            content: Text("Cancelled "),
+          ));
+    } catch (e) {
+      print('$e');
+    }
+  }
+
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {'amount': '200', 'currency': currency, 'payment_method_types[]': 'card'};
+      print(body);
+      var response = await http.post(Uri.parse('https://api.stripe.com/v1/payment_intents'), body: body, headers: {
+        'Authorization': 'Bearer sk_test_51KtYn3ELtEuzS0TK6Ai5tOJfRlhw2uSBOOVrhGXhGFiD0PTP5c2x0zH1VCKfInGwyx3a8jfXI7908DbbY6aea8de00r4lKg89e',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      });
+      print('Create Intent reponse ===> ${response.body.toString()}');
+      return jsonDecode(response.body);
+    } catch (err) {
+      print('err charging user: ${err.toString()}');
+    }
+  }
+}
